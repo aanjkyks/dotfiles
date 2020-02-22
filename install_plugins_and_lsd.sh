8fa9f4f6c6c817169162d07067500ed4a3de18d7
Ainis Antans <ainis.antans@gmail.com>
2020-02-22 11:52:14 +0200

    attempted script fix

--- install_plugins_and_lsd.sh
+++ install_plugins_and_lsd.sh
@@ -1,5 +1,15 @@
+echo -n Password: 
+read -s password
+
+sudo apt install git lolcat fortune cowsay zsh
+$password
+
+sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+
 cp ~/.zshrc ~/.zshrc.bak
-cp .zshrc ~/.zshrc
+echo "source ~/dotfiles/.zshrc" > ~/.zshrc
 
 wget https://github.com/Peltoche/lsd/releases/download/0.16.0/lsd_0.16.0_amd64.deb
 sudo dpkg -i lsd_0.16.0_amd64.deb
@@ -8,5 +18,3 @@ rm -f lsd_0.16.0_amd64.deb
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
-clear
-source ~/.zshrc
